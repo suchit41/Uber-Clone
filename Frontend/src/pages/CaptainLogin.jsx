04c548096acfc609f6a36d7258411d87685585d1
suchit41 <suchitpravinjain@gmail.com>
2026-06-13 02:38:42 +0530

    Frontend Setup

---
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CaptainLogin() {
+  return (
+    <div>
+      captain logim
+    </div>
+  )
+}
+
+export default CaptainLogin
---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserLogin() {
+  return (
+    <div>
+      User Login
+    </div>
+  )
+}
+
+export default UserLogin
